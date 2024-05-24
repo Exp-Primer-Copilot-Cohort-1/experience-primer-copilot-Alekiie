fc580646bae550de753d95c78c0adc295f9e5a34
Alexander Maina <96947422+Alekiie@users.noreply.github.com>
2024-05-24 09:53:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/views/skills.html',
+        controller: 'SkillsController',
+        controllerAs: 'skillsCtrl'
+    };
+}
